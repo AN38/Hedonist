531c230516662937103ce675c733d3a4aec3762f
AN38 <nikolskaya.a@chocolife.kz>
2019-12-08 20:51:12 +0600

    Email validation added

--- app/src/main/java/com/example/hedonist/LoginActivity.java
+++ app/src/main/java/com/example/hedonist/LoginActivity.java
@@ -1,27 +1,54 @@
 package com.example.hedonist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button buttonContinue;
+    private EditText editTextEmail;
+    private String login = "nikolskaya.a@rahmetapp.kz";
+    private int error = R.string.invalid_email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
         buttonContinue = findViewById(R.id.btn_continue);
+        editTextEmail = findViewById(R.id.et_email);
+
         buttonContinue.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, LoginActivityPassword.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                String editText = editTextEmail.getText().toString();
+                if (editText.equals(login)) {
+                    Toast.makeText(LoginActivity.this,editText,Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, LoginActivityPassword.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this,error,Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String userEmail = editTextEmail.getText().toString();
+        outState.putString("UserEmail", userEmail);
+
+    }
 }
+
--- app/src/main/java/com/example/hedonist/LoginActivityPassword.java
+++ app/src/main/java/com/example/hedonist/LoginActivityPassword.java
@@ -6,19 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivityPassword extends AppCompatActivity {
 
     private Button buttonLogIn;
+    String warningText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_password);
         buttonLogIn = findViewById(R.id.btn_log_in);
+        if(savedInstanceState != null) {
+            String editText = savedInstanceState.getString("UserEmail");
+            Toast.makeText(LoginActivityPassword.this, warningText, Toast.LENGTH_LONG).show();
+        }
         buttonLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(LoginActivityPassword.this, MenuScreen.class);
                 startActivity(intent);
             }
