da9c5f8241a09019e176396b913483a583eb69fd
AN38 <nikolskaya.a@chocolife.kz>
2019-12-09 12:04:24 +0600

    Change background color added

--- app/src/main/java/com/example/hedonist/LoginActivity.java
+++ app/src/main/java/com/example/hedonist/LoginActivity.java
@@ -8,12 +8,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button buttonContinue;
+    private ImageView marshmallowIcon;
+    private ImageView coffeeIcon;
+    private ImageView cupcakeIcon;
+    private LinearLayout login_activity_layout;
     private EditText editTextEmail;
     private String login = "nikolskaya.a@chocolife.kz";
     private int invalid_email_error = R.string.invalid_email;
@@ -23,9 +30,34 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-
+        coffeeIcon = findViewById(R.id.coffee_icon);
+        marshmallowIcon = findViewById(R.id.marshmallow_icon);
+        cupcakeIcon = findViewById(R.id.cupcake_icon);
         buttonContinue = findViewById(R.id.btn_continue);
         editTextEmail = findViewById(R.id.et_email);
+        login_activity_layout = findViewById(R.id.login_activity_layout);
+
+        coffeeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_activity_layout.setBackground(getDrawable(R.drawable.gradient_orange_violet));
+            }
+        });
+
+        marshmallowIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login_activity_layout.setBackground(getDrawable(R.drawable.gradient_purple_blue));
+            }
+        });
+
+        cupcakeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_activity_layout.setBackground(getDrawable(R.drawable.gradient_orange_violet));
+            }
+        });
+
 
         buttonContinue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +68,7 @@ public class LoginActivity extends AppCompatActivity {
                     intent.putExtra("User Email", editText);
                     startActivity(intent);
                 } else {
-                    Toast.makeText(LoginActivity.this,invalid_email_error,Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, invalid_email_error, Toast.LENGTH_LONG).show();
                 }
 
             }
